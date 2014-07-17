a24fe8bf24604814a10d7754bcca0dc23679883f
ayush1794 <ayush1794@gmail.com>
2014-07-17 00:56:33 +0000

    removed ffvideo added ffmpeg supprt

--- damn_at/transcoders/video/transcodervideo2image.py
+++ damn_at/transcoders/video/transcodervideo2image.py
@@ -1,12 +1,14 @@
 """Video to Image Transcoder """
-import os
-from ffvideo import VideoStream
+import os 
+import tempfile 
+import subprocess
+from PIL import Image
 from damn_at.pluginmanager import ITranscoder
 from damn_at.options import IntOption, IntVectorOption, expand_path_template
 
 class Video2ImageTranscoder(ITranscoder):
     """Generic Transcoder class for video2image"""
-    options = [IntOption(name='second', description='The second from which frame is to be extracted', default=-1),
+    options = [IntOption(name='second', description='The second from which frame is to be extracted', default=0),
         IntVectorOption(name='size', description='The size of output image in pixels', size=2, min=1, max=4096, default=(-1, -1))]
 
     convert_map = {"video/mp4" : {"image/png":options, "image/jpeg":options},
@@ -30,18 +32,20 @@ class Video2ImageTranscoder(ITranscoder):
                 target_mimetype.mimetype, asset_id, **options)
 
         try:
-            stream = VideoStream(file_descr.file.filename)
-            if options['second'] > int(stream.duration):
-                print "The video is smaller than output frame!", file_descr.file.filename
+            tmp = tempfile.NamedTemporaryFile(suffix = '.jpeg')
+            pro = subprocess.Popen(['ffmpeg', '-ss', str(options['second']),
+                '-i', file_descr.file.filename, '-t', '1', '-r', '1', tmp.name, '-y' ])
+            out, err = pro.communicate()
+            if pro.returncode != 0:
+                print('ffmpeg failed %s with error code %d' 
+                        %(file_descr.file.filename, pro.returncode), err)
                 return False
-            if options['second'] == -1:
-                image = stream.get_frame_at_sec(int(stream.duration/2)).image()
-            else:
-                image = stream.get_frame_at_sec(options['second']).image()
         except OSError:
             print "Cannot open video", file_descr.file.filename
             return False
 
+        image = Image.open(tmp.name)
+        
         full_path = os.path.join(dest_path, file_path)
         if not os.path.exists(os.path.dirname(full_path)):
             os.makedirs(os.path.dirname(full_path))
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ config = {
     'version': '0.1',
     'test_suite': 'tests.suite',
     'install_requires': ['Yapsy', 'Image', 'pylint', 'thrift', 'gitpython', 'fusepy',
-         'ffvideo', 'filemagic'],
+         'matplotlib', 'filemagic'],
     'packages': ['damn_at'],
     'scripts': [],
     'name': 'damn_at',
