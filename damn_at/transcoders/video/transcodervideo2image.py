24ff606e79137c433c5cb37d5af46da226ee6c2d
ayush1794 <ayush1794@gmail.com>
2014-08-05 09:15:40 +0530

    Error msg if video transcoder out of range [added]

--- damn_at/transcoders/video/transcodervideo2image.py
+++ damn_at/transcoders/video/transcodervideo2image.py
@@ -8,7 +8,7 @@ from damn_at.options import IntOption, IntVectorOption, expand_path_template
 
 class Video2ImageTranscoder(ITranscoder):
     """Generic Transcoder class for video2image"""
-    options = [IntOption(name='second', description='The second from which frame is to be extracted', default=0),
+    options = [IntOption(name='second', description='The second from which frame is to be extracted', default=-1, min=-1),
         IntVectorOption(name='size', description='The size of output image in pixels', size=2, min=1, max=4096, default=(-1, -1))]
 
     convert_map = {"video/mp4" : {"image/png":options, "image/jpeg":options},
@@ -31,10 +31,20 @@ class Video2ImageTranscoder(ITranscoder):
         file_path = expand_path_template(target_mimetype.template,
                 target_mimetype.mimetype, asset_id, **options)
 
+        time = file_descr.assets[0].metadata['duration'].string_value.split(':')
+        time = eval(time[0])*3600 + eval(time[1])*60 + eval(time[2])
+        if time < options['second']:
+            print "Not in range of video", file_descr.file.filename
+            return False
+
+        if options['second']==-1:
+            second = time/2
+        else:
+            second = options['second']
         try:
             tmp = tempfile.NamedTemporaryFile(suffix = '.jpeg')
-            pro = subprocess.Popen(['ffmpeg', '-ss', str(options['second']),
-                '-i', file_descr.file.filename, '-t', '1', '-r', '1', tmp.name, '-y' ])
+            pro = subprocess.Popen(['ffmpeg', '-ss', str(second), '-i',
+                file_descr.file.filename, '-t', '1', '-r', '1', tmp.name, '-y' ])
             out, err = pro.communicate()
             if pro.returncode != 0:
                 print('ffmpeg failed %s with error code %d' 
