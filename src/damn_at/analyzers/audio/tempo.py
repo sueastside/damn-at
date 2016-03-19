c8b014e46a804c184a169b33e314c255e444c2eb
goelakash <goelakash93@gmail.com>
2016-03-19 23:56:24 +0530

    Minor correction

--- src/damn_at/analyzers/audio/tempo.py
+++ src/damn_at/analyzers/audio/tempo.py
@@ -2,6 +2,6 @@ import os
 import sys
 import subprocess as sp
 
-p = sp.Popen(["TempoDetector", "single", "coldplay.m4a"],stdout=sp.PIPE)
+p = sp.Popen(["TempoDetector", "single", sys.argv[-1]],stdout=sp.PIPE)
 output = p.communicate()[0]
 print output.split()[1]
